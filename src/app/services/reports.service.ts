28959d13c73fcd04c8afc8c8e8c522cb3779159e
JuanMartinIwassjuk <jiwassjuk@frba.utn.edu.ar>
2025-06-18 20:33:13 -0300

    data reports

--- src/app/services/reports.service.ts
+++ src/app/services/reports.service.ts
@@ -15,16 +15,22 @@ export class ReporteService {
 
   getConsumoPorTipo(): ReporteConsumo {
     return {
-      labels: ['Consumo normal', 'Consumo elevado', 'Consumo nocturno', 'Pérdidas'],
-      data: [65, 20, 10, 5] 
+      labels: ['Mañana', 'Mediodía', 'Tarde', 'Noche'],
+      data: [38, 7, 20, 35] 
     };
   }
 
 
   getConsumoMensual(): ReporteConsumo {
     return {
-      labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio'],
-      data: [5300, 4800, 5100, 4700, 4900, 4600]
+      labels: [
+        'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+        'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+      ],
+      data: [
+        5300, 4800, 5100, 4700, 4900, 4600,
+        5000, 5200, 4950, 5100, 5050, 4800
+      ]
     };
   }
 }
