33de8c3c1821e6be81452c900067156b18c542af
matiasplanchuelo <mplanchuelo@fbra.utn.edu.ar>
2025-10-06 22:25:24 -0300

    cambios en modals mi-cuenta y gamificacion

--- src/app/components/gamification/gamification.component.ts
+++ src/app/components/gamification/gamification.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation, AfterViewInit, ViewChild, TemplateRef} from '@angular/core';
+import { AuthService } from '../../auth/serviceAuth/auth.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-gamification',
-  standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule],
   templateUrl: './gamification.component.html',
-  styleUrls: ['./gamification.component.css']
+  styleUrl: './gamification.component.css',
+  standalone: true,
+  encapsulation: ViewEncapsulation.None,
+  animations: [
+    trigger('modalAnimation', [
+      state('open', style({ opacity: 1, transform: 'scale(1)' })),
+      state('closed', style({ opacity: 0, transform: 'scale(0.8)' })),
+      transition('closed => open', [animate('300ms ease-out')]),
+      transition('open => closed', [animate('300ms ease-in')])
+    ])]
 })
-export class GamificationComponent {}
+
+export class GamificationComponent {
+
+  activeModal: string | null = null;
+  
+  constructor(private authService: AuthService, private router: Router){  }
+  
+  @ViewChild('medallasModal') medallasModal!: TemplateRef<any>;
+  
+  modalTemplates: { [key: string]: TemplateRef<any> } = {};
+  
+  ngAfterViewInit(): void {
+    // prevenir navegación en los anchors
+    const anchors = document.querySelectorAll('a[href="#"]');
+    anchors.forEach(anchor => {
+      anchor.addEventListener('click', (event) => event.preventDefault());
+    });
+  
+    this.modalTemplates = {
+      medallas: this.medallasModal,
+    };
+  }
+  
+  openModal(id: string) {
+    this.activeModal = id;
+  }
+  
+  closeModal() {
+    this.activeModal = null;
+  }
+
+}
